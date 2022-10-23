6afdb364d191bd946beab65ec90888d51d5185f9
Juanperezaa <sebas8041@hotmail.com>
2022-10-23 11:05:20 -0500

    Some fixes

--- zoo-Web/src/test/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO "SWAN" (id,name, sex, weight, age,height,arrival_date,fatherid,motherid) VALUES ('406b66ca-bcdd-49a0-8058-5373132c2bdb', 'Fernando', 'M', '4', '5','110','2020-09-22 12:14:13','eab9096b-f745-470b-9d04-a8b3184cf73a','6617d825-0278-4495-a7c0-8a6a4fec9987')
-INSERT INTO "SWAN" (id,name, sex, weight, age,height,arrival_date,fatherid,motherid) VALUES ('fb4837f2-bb4e-4be6-8609-621074c5cf0d', 'Carla', 'F', '4', '15','110','2020-09-22 12:14:13','1eb46ded-6101-3694-9f49-598db6bff142','59bf924c-0a30-354f-ae4a-ec7f9122eeb1')
-INSERT INTO "SWAN" (id,name, sex, weight, age,height,arrival_date,fatherid,motherid) VALUES ('e51af411-d22c-43f8-85c1-d40e771fbc9c', 'Eugenia', 'F', '4', '17','110','2020-09-22 12:14:13','46b92d40-4fbd-11ed-bdc3-0242ac120002','8b97ec58-4fbd-11ed-bdc3-0242ac120002')
-
