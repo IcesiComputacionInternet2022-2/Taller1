fb64d80e83cf5a5f600a46fbaa066fcc23ea6589
alexandersanchezjr <alexandersanchez.jr@outlook.com>
2022-10-22 21:26:05 -0500

    Hyena integration tests created with successful and unsuccessful cases and corresponding error cases

--- src/test/java/co/edu/icesi/calizoo/integration/HyenaIntegrationTests.java
+++ src/test/java/co/edu/icesi/calizoo/integration/HyenaIntegrationTests.java
@@ -1,17 +1,24 @@
 package co.edu.icesi.calizoo.integration;
 
 
+import co.edu.icesi.calizoo.constant.HyenaErrorCode;
 import co.edu.icesi.calizoo.dto.HyenaDTO;
+import co.edu.icesi.calizoo.error.exception.HyenaError;
+import co.edu.icesi.calizoo.integration.config.InitialDataConfig;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.SneakyThrows;
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
@@ -32,7 +39,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = { "spring.datasource.url=jdbc:h2:mem:testdb" }
+)
+@ActiveProfiles("test")
+
 public class HyenaIntegrationTests {
 
     private MockMvc mockMvc;
@@ -42,6 +54,9 @@ public class HyenaIntegrationTests {
 
     private ObjectMapper objectMapper;
 
+    private static final String HYENA_UUID = "5631cbd3-cf53-415f-bd06-4e995ee3c322";
+
+
     @BeforeEach
     private void init() {
         objectMapper = new ObjectMapper();
@@ -50,18 +65,154 @@ public class HyenaIntegrationTests {
     }
     @Test
     @SneakyThrows
-    public void createUserSuccessfully() {
+    public void createHyenaSuccessfully() {
         String body = parseResourceToString("createHyena.json");
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/hyenas")
                     .contentType(MediaType.APPLICATION_JSON)
                     .content(body)).andExpect(status().isOk())
                     .andReturn();
 
-        HyenaDTO userResult = objectMapper.readValue(result.getResponse().getContentAsString(),HyenaDTO.class);
+        HyenaDTO hyenaResult = objectMapper.readValue(result.getResponse().getContentAsString(),HyenaDTO.class);
 
-        assertThat(userResult,hasProperty("name",is("Alex")));
+        assertThat(hyenaResult,hasProperty("name",is("Alex")));
 
     }
+
+    @Test
+    @SneakyThrows
+    public void getHyenaSuccessfully() {
+
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.get("/hyenas/" + HYENA_UUID)
+                        .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        HyenaDTO hyenaResult = objectMapper.readValue(result.getResponse().getContentAsString(), HyenaDTO.class);
+
+        assertThat(hyenaResult, hasProperty("name", Matchers.is("Abby")));
+
+    }
+
+    @Test
+    @SneakyThrows
+    public void createHyenaWithWrongName() {
+        String body = parseResourceToString("createHyenaWithWrongName.json");
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/hyenas")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(body)).andExpect(status().isBadRequest())
+                .andReturn();
+
+        HyenaError hyenaError = objectMapper.readValue(result.getResponse().getContentAsString(), HyenaError.class);
+
+        assertThat(hyenaError, hasProperty("code", is(HyenaErrorCode.CODE_01)));
+        assertThat(hyenaError, hasProperty("message", is("Name should contains only letters and spaces. It should be between 1 and 120 characters long")));
+    }
+
+    @Test
+    @SneakyThrows
+    public void createHyenaWithArrivalDateAfterToday() {
+        String body = parseResourceToString("createHyenaWithArrivalDateAfterToday.json");
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/hyenas")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body)).andExpect(status().isBadRequest())
+                .andReturn();
+
+        HyenaError hyenaError = objectMapper.readValue(result.getResponse().getContentAsString(), HyenaError.class);
+
+        assertThat(hyenaError, hasProperty("code", is(HyenaErrorCode.CODE_02)));
+        assertThat(hyenaError, hasProperty("message", is("The arrival date should be before the current date")));
+    }
+
+    @Test
+    @SneakyThrows
+    public void createHyenaWithWrongAge() {
+        String body = parseResourceToString("createHyenaWithWrongAge.json");
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/hyenas")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body)).andExpect(status().isBadRequest())
+                .andReturn();
+
+        HyenaError hyenaError = objectMapper.readValue(result.getResponse().getContentAsString(), HyenaError.class);
+
+        assertThat(hyenaError, hasProperty("code", is(HyenaErrorCode.CODE_03)));
+        assertThat(hyenaError, hasProperty("message", is("Age should be between 1 and 25 years old according to hyenas aging. Please refer to wikipedia for more information")));
+    }
+
+    @Test
+    @SneakyThrows
+    public void createHyenaWithWrongWeight() {
+        String body = parseResourceToString("createHyenaWithWrongWeight.json");
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/hyenas")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body)).andExpect(status().isBadRequest())
+                .andReturn();
+
+        HyenaError hyenaError = objectMapper.readValue(result.getResponse().getContentAsString(), HyenaError.class);
+
+        assertThat(hyenaError, hasProperty("code",is(HyenaErrorCode.CODE_04)));
+        assertThat(hyenaError, hasProperty("message", is("Weight should be between 1 and 64 kg (kilograms according to hyenas weighting. Please refer to wikipedia for more information")));
+    }
+
+    @Test
+    @SneakyThrows
+    public void createHyenaWithWrongHeight() {
+        String body = parseResourceToString("createHyenaWithWrongHeight.json");
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/hyenas")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body)).andExpect(status().isBadRequest())
+                .andReturn();
+
+        HyenaError hyenaError = objectMapper.readValue(result.getResponse().getContentAsString(), HyenaError.class);
+
+        assertThat(hyenaError, hasProperty("code",is(HyenaErrorCode.CODE_05)));
+        assertThat(hyenaError, hasProperty("message", is("Height should be between 1 and 92 cm (centimeters) according to hyenas general height. Please refer to wikipedia for more information")));
+    }
+
+    @Test
+    @SneakyThrows
+    public void createHyenaWithWrongFather() {
+        String body = parseResourceToString("createHyenaWithWrongFather.json");
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/hyenas")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body)).andExpect(status().isBadRequest())
+                .andReturn();
+
+        HyenaError hyenaError = objectMapper.readValue(result.getResponse().getContentAsString(), HyenaError.class);
+
+        assertThat(hyenaError, hasProperty("code",is(HyenaErrorCode.CODE_06)));
+        assertThat(hyenaError, hasProperty("message", is("Father gender should be male")));
+    }
+
+    @Test
+    @SneakyThrows
+    public void createHyenaWithWrongMother() {
+        String body = parseResourceToString("createHyenaWithWrongMother.json");
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/hyenas")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body)).andExpect(status().isBadRequest())
+                .andReturn();
+
+        HyenaError hyenaError = objectMapper.readValue(result.getResponse().getContentAsString(), HyenaError.class);
+
+        assertThat(hyenaError, hasProperty("code",is(HyenaErrorCode.CODE_07)));
+        assertThat(hyenaError, hasProperty("message", is("Mother gender should be female")));
+    }
+
+    @Test
+    @SneakyThrows
+    public void createHyenaWihTakenName() {
+        String body = parseResourceToString("createHyenaWithTakenName.json");
+        MvcResult result = mockMvc.perform(MockMvcRequestBuilders.post("/hyenas")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body)).andExpect(status().isBadRequest())
+                .andReturn();
+
+        HyenaError hyenaError = objectMapper.readValue(result.getResponse().getContentAsString(), HyenaError.class);
+
+        assertThat(hyenaError, hasProperty("code",is(HyenaErrorCode.CODE_08)));
+        assertThat(hyenaError, hasProperty("message", is("Name is already taken")));
+    }
+
     @SneakyThrows
     private String parseResourceToString (String classPath) {
         Resource resource = new ClassPathResource(classPath);
